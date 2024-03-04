2d65b07e07d9d4f5d5cb0c9e12481b39d2373a55
Servin13 <20030367@itcelaya.edu.mx>
2024-03-04 10:11:54 -0600

    Proyecto 2 completo

---
+++ MVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+        /*public string Index()
+        {
+            return "This is my default action...";
+        }*/
+
+        //Get: /HelloWorld/Welcome/
+        //Requieres using System.Text.Encodings.Web
+        //Get: /HelloWorld/Welcome?name=angel&numtimes=5
+        /*public string Welcome(string name, int numTimes=1)
+        {
+            //return "This is the welcome action method...";
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }*/
+    }
+}
